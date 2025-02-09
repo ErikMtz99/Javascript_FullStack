6cd39e32f8d23c6ecad3cdfbe8396b5d807a0397
Erik Martinez <ferik.mtz@outlook.com>
2025-02-09 15:04:19 -0600

    exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -8,12 +8,20 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <>
+      <p>{props.part} {props.exercise}</p>
+    </>
+  )
+}
+
 const Content = (props) => {
   return (
     <>
-      <p>{props.p1} {props.ex1}</p>
-      <p>{props.p2} {props.ex2}</p>
-      <p>{props.p3} {props.ex3}</p>
+      <Part part = {props.p1} exercise = {props.ex1}/>
+      <Part part = {props.p2} exercise = {props.ex2}/>
+      <Part part = {props.p3} exercise = {props.ex3}/>
     </>
   )
 }
