c6893f4377325bb1e18c2f7a8c20d7ab938f7b8b
Erik Martinez <ferik.mtz@outlook.com>
2025-03-10 19:04:06 -0600

    excercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -37,6 +37,8 @@ const App = () => {
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>Average: {(good-bad)/(good+bad+neutral)}</p>
+      <p>Positive: {(good/(good+bad+neutral))*100} %</p>
     </div>
   )
 }
