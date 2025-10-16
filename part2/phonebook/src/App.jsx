6209c57abe67afaa06c3bb13f593334651ec1fcb
Erik Martinez <ferik.mtz@outlook.com>
2025-10-16 14:02:17 -0600

    excercise 2.10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,15 +5,52 @@ const phoneNumbersArray = [
   {id: 2, name: 'Juan', phone: 1234567890 },
 ]
 
+const FilterPerson = (props) => {
+    let foundPhoneNumbers = []
+    foundPhoneNumbers = props.phoneNumbers.filter(value => (value.name).toLowerCase() === props.newName.toLowerCase())
+    return(
+      <div>
+        Enter name to search: <input value={props.newName} onChange={props.filterFunc}/>
+        {foundPhoneNumbers.map(entry => <li key={entry.id}>{entry.name} : {entry.phone}</li>)}
+      </div>
+    )
+  }
+
+const PersonForm = (props) => {
+    return (
+      <div>
+        <form onSubmit={props.addNewEntry} value>
+        <div>
+          name: <input value={props.newName} onChange={props.handleNewName}/>
+        </div>
+        <div>
+          number: <input value={props.newNumber} onChange={props.handleNewNumber}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      </div>
+    )
+  }
+
+  const ShowPersons = (props) => {
+    return (
+      <div>
+        {props.phoneNumbers.map(entry => <li key={entry.id}>{entry.name} : {entry.phone}</li>)}
+      </div>
+    )
+  }
+
 const App = () => {
   const [phoneNumbers, setPhoneNumbers] = useState(phoneNumbersArray)
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('0000000000')
-  let foundPhoneNumbers = []
+  
 
   const addNewEntry = (event) => {
     event.preventDefault();
-    if((phoneNumbers.filter((number) => number.name === newName)).length === 0) // if newName is not in array (new array lenght is zero because there is no match)
+    if((phoneNumbers.filter((number) => number.name.toLowerCase() === newName.toLowerCase())).length === 0) // if newName is not in array (new array lenght is zero because there is no match)
     {
       console.log('submiting new person...')
       setPhoneNumbers(phoneNumbers.concat({id:phoneNumbers.length + 1, name:newName, phone:newNumber}))
@@ -37,40 +74,19 @@ const App = () => {
     console.log('filtering name  ' + event.target.value);
   }
 
-  const FilterPerson = () => {
-    foundPhoneNumbers = phoneNumbers.filter(value => (value.name).toLowerCase() === newName.toLowerCase())
-    return(
-      <div>
-        {foundPhoneNumbers.map(entry => <li key={entry.id}>{entry.name} : {entry.phone}</li>)}
-      </div>
-    )
-  }
-
   return (
     <div>
       <h1>Phonebook</h1>
+
       <h2> Search phonebook by name: </h2>
-      <div>
-        Enter name to search: <input value={newName} onChange={handleFilterName}/>
-        <FilterPerson> List: </FilterPerson>
-      </div>
+      <FilterPerson phoneNumbers = {phoneNumbers} newName = {newName} filterFunc = {handleFilterName}/>
+
       <h2> Add new: </h2>
-      <form onSubmit={addNewEntry} value>
-        <div>
-          name: <input value={newName} onChange={handleNewName}/>
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNewNumber}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm newName = {newName} newNumber = {newNumber} handleNewNumber = {handleNewNumber} handleNewName ={handleNewName} addNewEntry = {addNewEntry}/>
+
       <h2>Numbers</h2>
-      <div>
-        {phoneNumbers.map(entry => <li key={entry.id}>{entry.name} : {entry.phone}</li>)}
-      </div>
-      ...
+      <ShowPersons phoneNumbers={phoneNumbers}/>
+
     </div>
   )
 }
