4f834b66089b9df101cc6cf39d7b88387d0dd1ed
Erik Martinez <ferik.mtz@outlook.com>
2025-10-22 18:00:32 -0600

    excercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,5 @@
-import { useState } from 'react'
-
-const phoneNumbersArray = [
-  {id: 1, name: 'Erik', phone: 1111111111 }, 
-  {id: 2, name: 'Juan', phone: 1234567890 },
-]
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const FilterPerson = (props) => {
     let foundPhoneNumbers = []
@@ -11,7 +7,7 @@ const FilterPerson = (props) => {
     return(
       <div>
         Enter name to search: <input value={props.newName} onChange={props.filterFunc}/>
-        {foundPhoneNumbers.map(entry => <li key={entry.id}>{entry.name} : {entry.phone}</li>)}
+        {foundPhoneNumbers.map(entry => <li key={entry.id}>{entry.name} : {entry.number}</li>)}
       </div>
     )
   }
@@ -37,15 +33,20 @@ const PersonForm = (props) => {
   const ShowPersons = (props) => {
     return (
       <div>
-        {props.phoneNumbers.map(entry => <li key={entry.id}>{entry.name} : {entry.phone}</li>)}
+        {props.phoneNumbers.map(entry => <li key={entry.id}>{entry.name} : {entry.number}</li>)}
       </div>
     )
   }
 
 const App = () => {
-  const [phoneNumbers, setPhoneNumbers] = useState(phoneNumbersArray)
+  const [phoneNumbers, setPhoneNumbers] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('0000000000')
+  
+  useEffect(() => {
+    console.log('effect')
+    axios.get('http://localhost:3001/persons').then(response => {setPhoneNumbers(response.data)}) 
+  }, []) //effect only run after 1st component's render
   
 
   const addNewEntry = (event) => {
@@ -53,7 +54,7 @@ const App = () => {
     if((phoneNumbers.filter((number) => number.name.toLowerCase() === newName.toLowerCase())).length === 0) // if newName is not in array (new array lenght is zero because there is no match)
     {
       console.log('submiting new person...')
-      setPhoneNumbers(phoneNumbers.concat({id:phoneNumbers.length + 1, name:newName, phone:newNumber}))
+      setPhoneNumbers(phoneNumbers.concat({id:phoneNumbers.length + 1, name:newName, number:newNumber}))
     }
     else { // if newName is already in array
       alert(`${newName} is already added to phonebook`)
--- part2/practice/src/App.jsx
+++ part2/practice/src/App.jsx
@@ -1,52 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Note from './components/Note.jsx'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
 
-const App = (props) => {
-
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('a new note...')
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios.get('http://localhost:3001/notes').then((response) => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    })
+  }, [])
+  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      important: Math.random() < 0.5,
-      id: String(notes.lenght + 1),
+      important: Math.random() > 0.5,
+      id: String(notes.length + 1),
     }
 
     setNotes(notes.concat(noteObject))
     setNewNote('')
-
-    console.log('button clicked', event.target)
   }
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
     setNewNote(event.target.value)
   }
 
-  //If showAll is true, then notes is shown. If false, filtered notes are shown.
-  const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
   return (
     <div>
       <h1>Notes</h1>
-
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
         </button>
       </div>
       <ul>
-        {notesToShow.map(note => <Note key={notes.id} note ={note} />)}
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
       </ul>
-
       <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange}/>
+        <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
     </div>
