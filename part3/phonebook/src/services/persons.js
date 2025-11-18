bd302250b0b5f3d4551a72dc7601ef72a769bb87
Erik Martinez <ferik.mtz@outlook.com>
2025-11-17 20:39:50 -0700

    exercise 3.17

--- part3/phonebook/src/services/persons.js
+++ part3/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'http://localhost:3001/api/persons' //Last changes were from exercise 3.17
 
 /*
 const getAll = () => {
